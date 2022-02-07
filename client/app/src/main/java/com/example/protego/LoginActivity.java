1e40270c79afdd84c01fdbe1d7572aba52718601
Chelsea Lantigua <39841240+chelseaL01@users.noreply.github.com>
2022-02-06 21:48:28 -0500

    Design for the Login page

---
+++ client/app/src/main/java/com/example/protego/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.protego;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.protego.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
