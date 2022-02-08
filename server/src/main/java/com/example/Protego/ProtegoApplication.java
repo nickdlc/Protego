59b7d3b96ad77d5cf3c932fae00994513d63761b
nickdlc <njdlc22@gmail.com>
2022-02-07 21:41:46 -0500

    Add initial Spring Boot setup

---
+++ server/src/main/java/com/example/Protego/ProtegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Protego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtegoApplication.class, args);
+	}
+
+}
