a67595522f73f570f4cb3652ea20a7d4815e43bd
Greg <gkimatov@gmail.com>
2022-02-20 17:50:04 -0500

    edit res files

--- client/app/src/main/java/com/example/protego/SplashScreenActivity.java
+++ client/app/src/main/java/com/example/protego/SplashScreenActivity.java
@@ -3,15 +3,23 @@ package com.example.protego;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    public static final String TAG = "SplashScreenActivity";
     Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_splash);
 
         if(getSupportActionBar() != null)
