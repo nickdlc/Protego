21d58f784c1f1c9cd1c8804c74ac0f1cde1cc153
Andy Gonzalez <gonzalezandy21@gmail.com>
2022-02-20 21:25:18 -0500

    Added Java File For Dashboard

--- client/app/src/main/java/com/example/protego/PatientDashboardActivity.java
+++ client/app/src/main/java/com/example/protego/PatientDashboardActivity.java
@@ -2,8 +2,10 @@ package com.example.protego;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 
 public class PatientDashboardActivity extends AppCompatActivity{
     public static class PatientInfo {
@@ -17,9 +19,10 @@ public class PatientDashboardActivity extends AppCompatActivity{
     }
 
     PatientInfo[] patientData = new PatientInfo[] {
-            new PatientInfo("Heart Rate:", "Fast"),
-            new PatientInfo("Blood Pressure:", "Yes"),
-            new PatientInfo("Temperature:", "Maybe")};
+            new PatientInfo("Heart Rate:", "87 Bpm"),
+            new PatientInfo("Blood Pressure:", "120.5"),
+            new PatientInfo("Temperature:", "97.8"),
+            new PatientInfo("Blood-Type:","O-")};
 
 
 
@@ -29,8 +32,16 @@ public class PatientDashboardActivity extends AppCompatActivity{
         for(PatientInfo info: patientData){
             TextView dataTitle = new TextView(getBaseContext());
             dataTitle.setText(info.title);
+            dataTitle.setTextSize(28);
+            dataTitle.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
+
+
             TextView dataDetails = new TextView(getBaseContext());
-            dataTitle.setText(info.details);
+            dataDetails.setText(info.details);
+            dataDetails.setTextSize(28);
+            dataDetails.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f));
+            dataDetails.setTypeface(null, android.graphics.Typeface.BOLD);
+            dataDetails.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
 
             LinearLayout newLayout = new LinearLayout(getBaseContext());
             newLayout.setOrientation(LinearLayout.HORIZONTAL);
