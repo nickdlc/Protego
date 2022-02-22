9544b155c33ea4f420c01f0187a8846ab08dcf7e
Greg <gkimatov@gmail.com>
2022-02-21 21:49:25 -0500

    fix newline bug

--- client/app/src/main/java/com/example/protego/SignupActivity.java
+++ client/app/src/main/java/com/example/protego/SignupActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
