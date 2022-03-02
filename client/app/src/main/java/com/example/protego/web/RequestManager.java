f9ab7e006ccdf34231875f6af221588100029917
kareem-ib <kareem315@gmail.com>
2022-03-02 02:20:04 -0500

    Add generic get and post requests with one instance of RequestManager

--- client/app/src/main/java/com/example/protego/web/RequestManager.java
+++ client/app/src/main/java/com/example/protego/web/RequestManager.java
@@ -1,19 +1,38 @@
 package com.example.protego.web;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class RequestManager {
-    private static final String TAG = "RequestManager";
-    // private static final String prefixURL = "127.0.0.1:8000";
-    private static final String prefixURL = "192.168.1.152:8000"; // for kareem
+import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+// Our manager to make requests to the server
+public class RequestManager {
+    private static boolean areYouKareem = true;
+
+    private static final String TAG = "RequestManager";
+    public static final String prefixURL = areYouKareem ? "192.168.1.152:8000" : "127.0.0.1:8000";
 
     private static RequestManager instance = null;
 
     private static RequestQueue queue;
+
+    public enum RequestType {
+        GET, POST;
+    }
 
     private RequestManager(Context context) {
         queue = Volley.newRequestQueue(context.getApplicationContext());
@@ -30,5 +49,76 @@ public class RequestManager {
             throw new IllegalStateException(RequestManager.class.getSimpleName() +
                     " is not initialized, call getInstance(...) first");
         return instance;
+    }
+
+    public void postRequest(Map<String, Object> paramList, String uri, final RequestListener<String> listener)
+    {
+        StringRequest request = new StringRequest(Request.Method.POST, uri, new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        // Request completed
+                        Log.d(TAG + ": ", "postRequest:recievedResponse : " + response.toString());
+                        if (null != response.toString())
+                            listener.getResult(response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Failed to make the request
+                        if (null != error.networkResponse)
+                        {
+                            Log.d(TAG + ": ", "Error Response code: " + error.networkResponse.statusCode);
+                            Log.e(TAG, "Error request : ", error);
+                            listener.getResult(null);
+                        }
+                    }
+                }) {
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8";
+                    }
+
+                    @Override
+                    public byte[] getBody() {
+                        String paramBody = new JSONObject(paramList).toString();
+                        return paramBody.getBytes(StandardCharsets.UTF_8);
+                    }
+                };
+        queue.add(request);
+    }
+
+    public void getRequest(Map<String, String> urlParams, String uri, final RequestListener<String> listener)
+    {
+        Log.d(TAG, "URL: " + uri);
+        StringRequest request = new StringRequest(Request.Method.GET, uri,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        // Request completed
+                        Log.d(TAG + ": ", "postRequest:recievedResponse : " + response.toString());
+                        if (null != response)
+                            listener.getResult(response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Failed to make the request
+                        if (null != error.networkResponse)
+                        {
+                            Log.d(TAG + ": ", "Error Response code: " + error.networkResponse.statusCode);
+                        }
+                        Log.e(TAG, "Error request : ", error);
+                        listener.getResult(null);
+                    }
+                });
+        queue.add(request);
     }
 }
