eaca266399aa615b3c5de4a8fa65e0a325b57dcb
Chelsea Lantigua <39841240+chelseaL01@users.noreply.github.com>
2022-03-02 00:33:26 -0500

    Add navigation bar to patient activities + Add patient name to navbar

--- client/app/src/main/java/com/example/protego/PatientDashboardActivity.java
+++ client/app/src/main/java/com/example/protego/PatientDashboardActivity.java
@@ -1,18 +1,25 @@
 package com.example.protego;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.view.ViewGroup.LayoutParams;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+        import android.view.ViewGroup.LayoutParams;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
 
 public class PatientDashboardActivity extends AppCompatActivity{
+    //input fields
+    private Button button;
+    private static String Name;
+
+
     public static class PatientInfo {
         private final String title;
         private final String details;
@@ -60,12 +67,42 @@ public class PatientDashboardActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_dashboard);
 
+
         RecyclerView recyclerView = findViewById(R.id.patientDataRecyclerView);
+
+        // update the user's name based on their profile information
+        //TODO: update this name according to the database to get the patient's first name
+        Name = "Example";
 
         setUpPatientInfo();
 
         PatientDashboardRecyclerViewAdapter adapter = new PatientDashboardRecyclerViewAdapter(this,patientData);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //TODO : update the connection, the View Doctors button is connected to the View Notes Activity to test it.
+        connectButtonToActivity(R.id.viewDoctorsButton, PatientNotesActivity.class);
+
     }
+
+    // navigate to next activity
+    private void connectButtonToActivity(Integer buttonId, Class nextActivityClass) {
+
+        button = findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), nextActivityClass);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    //function to call from the navbar fragment class to update the navbar according to the patients first name
+    public static String getName(){
+        return Name;
+    }
+
 }
