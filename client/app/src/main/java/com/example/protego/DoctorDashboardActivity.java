57b13e41de13874e7607d5f5320a668745470cdc
kareem-ib <kareem315@gmail.com>
2022-03-02 23:32:13 -0500

    Get doctor information from server on dashboard

--- client/app/src/main/java/com/example/protego/DoctorDashboardActivity.java
+++ client/app/src/main/java/com/example/protego/DoctorDashboardActivity.java
@@ -6,15 +6,27 @@ import androidx.fragment.app.FragmentContainerView;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.example.protego.web.ServerAPI;
+import com.example.protego.web.ServerRequest;
+import com.example.protego.web.ServerRequestListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DoctorDashboardActivity extends AppCompatActivity{
+    public static final String TAG = "DoctorActivity";
+
     //input fields
     private Button button;
+    private JSONObject doctorInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,8 @@ public class DoctorDashboardActivity extends AppCompatActivity{
         //Connects the View Patients button to the View Patients Activity
         connectButtonToActivity(R.id.DoctorScanQRCodeButton, DoctorScanQRCodeActivity.class);
 
+        // manually write in doctor uid for now
+        getDoctorInfo("gdI3CjRaCHRo9rn1PKfB");
     }
 
     // navigate to next activity
@@ -41,5 +55,31 @@ public class DoctorDashboardActivity extends AppCompatActivity{
             }
         });
     }
+
+    private void getDoctorInfo(String duid) {
+        ServerAPI.getDoctor(duid, new ServerRequestListener() {
+            @Override
+            public void recieveCompletedRequest(ServerRequest req) {
+                if (req.getResult().equals("")) {
+                    Log.d(TAG, "req recieved for doctor : " + req.getResult().toString());
+
+                    try {
+                        doctorInfo = req.getResultJSON();
+
+                        Log.d(TAG, "info first name : " + doctorInfo.getString("firstName"));
+
+                        // To get more info
+                        // doctorInfo.getString("firstName")
+                        // doctorInfo.getString("lastName")
+                    } catch (JSONException e) {
+                        Log.e(TAG, "could not recieve doctor info : ", e);
+                    }
+                } else {
+                    Log.d(TAG, "failed to get doctor : " + req.toString());
+                }
+            }
+        });
+    }
+
 }
 
