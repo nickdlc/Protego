505a7b40b5184f94c5826e1b97d54140967b33d1
nickdlc <njdlc22@gmail.com>
2022-03-05 21:44:13 -0500

    Add QR code generator to server

---
+++ server/src/main/java/com/example/Protego/QRCodeGenerator/QRCodeController.java
@@ -0,0 +1,28 @@
+package com.example.Protego.QRCodeGenerator;
+
+import com.google.zxing.WriterException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+@RestController
+public class QRCodeController {
+    @GetMapping("/qrcode")
+    public BufferedImage getQRCode(@RequestParam("patient") String puid) {
+        try {
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+
+            // will likely set a default width and height based on the QR code screen
+            return qrCodeGenerator.generateQRCode(puid, 250, 250);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
