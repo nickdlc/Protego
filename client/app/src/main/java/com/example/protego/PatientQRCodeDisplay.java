85565c5dac9d990e424b3a0e85ca2429731b70cf
nickdlc <njdlc22@gmail.com>
2022-03-16 00:44:07 -0400

    Modify patient QR code display

--- client/app/src/main/java/com/example/protego/PatientQRCodeDisplay.java
+++ client/app/src/main/java/com/example/protego/PatientQRCodeDisplay.java
@@ -1,12 +1,58 @@
 package com.example.protego;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.LruCache;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.example.protego.web.Endpoint;
+import com.example.protego.web.RequestManager;
+import com.example.protego.web.UriFormatter;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PatientQRCodeDisplay extends AppCompatActivity {
+    public static final String TAG = "PatientQRCodeDisplay";
+    private FirebaseAuth mAuth;
+
+    private NetworkImageView nv;
+    private ImageLoader imageLoader;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_qr_code_display);
+
+        NetworkImageView nv = (NetworkImageView) findViewById(R.id.qrCode);
+        RequestQueue q = RequestManager.getRequestQueue();
+        imageLoader = new ImageLoader(
+                q,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+
+        Map<String, String> map = new HashMap<>();
+        map.put("puid", mAuth.getCurrentUser().getUid());
+        map.put("width", "250");
+        map.put("height", "250");
+
+        nv.setDefaultImageResId(android.R.drawable.spinner_background);
+        nv.setImageUrl(UriFormatter.formatUrl(Endpoint.GET_QR_CODE, map), imageLoader);
     }
 }
--- client/app/src/main/java/com/example/protego/web/Endpoint.java
+++ client/app/src/main/java/com/example/protego/web/Endpoint.java
@@ -2,6 +2,7 @@ package com.example.protego.web;
 
 public enum Endpoint {
     GET_PATIENT("patient"), GET_DOCTOR("doctor"),
+    GET_QR_CODE("qrCode"),
 
     POST_ASSIGN("assign"), POST_NOTE("note"),
     POST_MEDICALINFO("medicalinfo"), POST_MEDICATION("medication");
