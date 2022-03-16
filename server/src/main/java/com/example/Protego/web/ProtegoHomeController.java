23a7f70f9b728f3c58b851d282d58f976c58e459
kareem-ib <kareem315@gmail.com>
2022-03-16 19:41:39 -0400

    Create endpoints in the server

--- server/src/main/java/com/example/Protego/web/ProtegoHomeController.java
+++ server/src/main/java/com/example/Protego/web/ProtegoHomeController.java
@@ -3,6 +3,8 @@ package com.example.Protego.web;
 import com.example.Protego.FirebaseAttributes.FirebaseAttributes;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
@@ -101,6 +103,29 @@ public class ProtegoHomeController {
         return note;
     }
 
+    @GetMapping("/medicalInfo")
+    public MedicalInfo getMedicalInfo(@RequestParam("patient") String puid) {
+        try {
+            // Asynchronously retrieve multiple documents
+            ApiFuture<QuerySnapshot> future =
+                    FirebaseAttributes.firestore.collection("users")
+                                            .document(puid)
+                                            .collection("MedicalInfo").get();
+                                            //.orderBy("date", Query.Direction.DESCENDING).limit(1).get();
+
+            // Just grab the top element for now
+            DocumentSnapshot document = future.get().getDocuments().get(0);
+
+            return document.toObject(MedicalInfo.class);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     @PostMapping("/medicalInfo")
     public MedicalInfo postMedicalInfo(@RequestBody MedicalInfo medInfo) {
         // Add medInfo to Firestore and return it
@@ -119,5 +144,26 @@ public class ProtegoHomeController {
                         .add(medication);
 
         return medication;
+    }
+
+    @PostMapping("/generatePatientInfo")
+    public MedicalInfo generatePatientInfo(@RequestBody Patient patient) {
+        String puid = patient.getPatientID();
+        MedicalInfo medInfo = new MedicalInfo();
+        medInfo.setHeartRate(randNum());
+        medInfo.setBloodPressure(randNum() + 40 + "/" + randNum());
+        medInfo.setBloodType("A+");
+        medInfo.setHeightIN(randNum() - 20);
+        medInfo.setWeight(randNum() + 60);
+        // Add medication to Firestore and use it
+        FirebaseAttributes.firestore.collection("users")
+                .document(puid).collection("MedicalInfo")
+                .add(medInfo);
+
+        return medInfo;
+    }
+
+    private Integer randNum() {
+        return (int)Math.floor(Math.random() * 40) + 60;
     }
 }
