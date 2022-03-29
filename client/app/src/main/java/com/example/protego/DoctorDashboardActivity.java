3ca3cbf94b58ce74fbdccdcac5987909e4875f02
nickdlc <njdlc22@gmail.com>
2022-03-29 01:18:02 -0400

    Add QR scanner for doctors

--- client/app/src/main/java/com/example/protego/DoctorDashboardActivity.java
+++ client/app/src/main/java/com/example/protego/DoctorDashboardActivity.java
@@ -1,5 +1,7 @@
 package com.example.protego;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentContainerView;
 
@@ -18,21 +20,79 @@ import com.example.protego.web.ServerAPI;
 import com.example.protego.web.ServerRequest;
 import com.example.protego.web.ServerRequestListener;
 import com.example.protego.web.schemas.DoctorDetails;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.client.android.Intents;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DoctorDashboardActivity extends AppCompatActivity{
     public static final String TAG = "DoctorActivity";
 
     //input fields
     private Button button;
+    private Button scanBtn;
     private DoctorDetails doctorInfo;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     //to store the doctor's last name
     public static String lastName;
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(
+            new ScanContract(),
+            result -> {
+                if (result.getContents() == null) {
+                    Intent originalIntent = result.getOriginalIntent();
+                    if (originalIntent == null) {
+                        Log.d(TAG, "Cancelled scan");
+                        Toast.makeText(DoctorDashboardActivity.this,
+                                "Cancelled the scan",
+                                Toast.LENGTH_LONG).show();
+                    } else if (originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                        Log.d(TAG, "Cancelled scan due to missing camera permissions");
+                        Toast.makeText(DoctorDashboardActivity.this,
+                                "Cancelled due to missing camera permissions",
+                                Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    String duid = mAuth.getCurrentUser().getUid();
+                    String puid = result.getContents();
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("duid", duid);
+                    data.put("puid", puid);
+                    data.put("active", true);
+                    db.collection("ConnectionRequest")
+                            .add(data)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "Successfully added request to Firestore with id"
+                                            + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e(TAG, "Error adding request", e);
+                                }
+                            });
+
+                    Log.d(TAG, "Scan successful");
+                    Toast.makeText(DoctorDashboardActivity.this,
+                            "Scan successful. The patient has received your connection request.",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +101,28 @@ public class DoctorDashboardActivity extends AppCompatActivity{
 
         doctorInfo = new DoctorDetails();
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         // update the user's last name based on their profile information
         getDoctorLastName(mAuth.getCurrentUser().getUid());
 
-        //Connects the Scan QR Code button to the QR Code activity
-        connectButtonToActivity(R.id.DoctorViewPatientsButton, DoctorViewPatientsActivity.class);
         //Connects the View Patients button to the View Patients Activity
-        connectButtonToActivity(R.id.DoctorScanQRCodeButton, DoctorScanQRCodeActivity.class);
+        connectButtonToActivity(R.id.DoctorViewPatientsButton, DoctorViewPatientsActivity.class);
+        //Connects the Scan QR Code button to the QR Code activity
+        //connectButtonToActivity(R.id.DoctorScanQRCodeButton, DoctorScanQRCodeActivity.class);
+
+        // Options for QR code scanner
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Scan a patient's QR code.");
+        options.setOrientationLocked(false);
+
+        scanBtn = findViewById(R.id.DoctorScanQRCodeButton);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                barcodeLauncher.launch(options);
+            }
+        });
 
         // manually write in doctor uid for now
         //getDoctorInfo(mAuth.getCurrentUser().getUid());
