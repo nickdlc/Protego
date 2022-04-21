616de6908deae196b9fabc3fa2cc1eb7dea42773
nickdlc <njdlc22@gmail.com>
2022-04-20 23:27:07 -0400

    Display doctor's workplace name and address in patient's view of their info

--- client/app/src/main/java/com/example/protego/PatientViewDoctorProfile.java
+++ client/app/src/main/java/com/example/protego/PatientViewDoctorProfile.java
@@ -21,6 +21,8 @@ public class PatientViewDoctorProfile extends AppCompatActivity {
     private Button btnReturn;
     private Button btnCancelConnection;
     private TextView tvDoctorFullName;
+    private TextView tvDoctorWorkplaceName;
+    private TextView tvDoctorAddress;
     private FirebaseAuth mAuth;
 
     @Override
@@ -30,15 +32,18 @@ public class PatientViewDoctorProfile extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        tvDoctorFullName = findViewById(R.id.tvDoctorFullName);
-
         Bundle extras = getIntent().getExtras();
         String doctorFullName = "test";
         if (extras != null) {
             doctorFullName = "Dr. " + extras.getString("firstName") + " " + extras.getString("lastName");
         }
 
+        tvDoctorFullName = findViewById(R.id.tvPatientViewDoctorProfileFullName);
         tvDoctorFullName.setText(doctorFullName);
+        tvDoctorWorkplaceName = findViewById(R.id.tvPatientViewDoctorProfileWorkplaceName);
+        tvDoctorWorkplaceName.setText(extras.getString("workplaceName"));
+        tvDoctorAddress = findViewById(R.id.tvPatientViewDoctorProfileAddress);
+        tvDoctorAddress.setText(extras.getString("address"));
 
         btnReturn = findViewById(R.id.returnFromPatientViewDoctorProfile);
         btnReturn.setOnClickListener(new View.OnClickListener() {
